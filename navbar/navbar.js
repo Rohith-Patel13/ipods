f5a415d423a0b0aa86b65098025a614e9772b379
Rohith patel <rohithgithub999@gmail.com>
2023-07-28 13:42:37 +0000

    media queries

--- navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-let barsCrossIdEl = document.getElementById("barsCrossId");
-let linksContainerIdEl = document.getElementById("linksContainerId");
-barsCrossIdEl.addEventListener("click", () => {
-  barsCrossIdEl.classList.toggle("fa-x");
-  linksContainerIdEl.classList.toggle("linksContainerDynamicallyShow");
-});
